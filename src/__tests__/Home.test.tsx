3c5757ad276fe40afd222d11387fafa50c4da666
joshborseth <joshua@0footprint.net>
2023-04-14 11:07:04 -0700

    add loading state for pokemon

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,9 +1,24 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Home from "../pages";
+import Home, { type Pokemon } from "../pages";
 
 test("there is an h1 on the home page", () => {
   render(<Home />);
   const h1 = screen.getByText("Pokemon List");
   expect(h1).toBeDefined();
+});
+
+test("we are rendering correct data from the api", async () => {
+  const fetchPokemon = async () => {
+    const res = await fetch(
+      "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+    );
+    const data = (await res.json()) as Pokemon[];
+    return data;
+  };
+  const pokemon = await fetchPokemon();
+  expect(pokemon).toBeDefined();
+  expect(pokemon.length).toBeGreaterThan(0);
+  if (!pokemon[0]) throw new Error("pokemon[0] is undefined");
+  expect(pokemon[0].name).toBe("Bulbasaur");
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export type Pokemon = {
 };
 
 const Home: NextPage = () => {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [pokemon, setPokemon] = useState<Pokemon[] | null>(null);
   useEffect(() => {
     const fetchPokemon = async () => {
       const res = await fetch(
@@ -30,21 +30,23 @@ const Home: NextPage = () => {
       <div>
         <h1 className="py-10 text-center text-4xl font-bold">Pokemon List</h1>
         <div className="grid grid-cols-5 justify-center gap-10">
-          {pokemon.map((p) => {
-            return (
-              <a
-                className="flex flex-col justify-between border-2"
-                href={`/pokemon/${p.id}`}
-                key={p.id}
-              >
-                <img
-                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${p.image}`}
-                  alt={p.name}
-                />
-                <p className="text-center">{p.name}</p>
-              </a>
-            );
-          })}
+          {!pokemon && <p>Loading...</p>}
+          {pokemon &&
+            pokemon.map((p) => {
+              return (
+                <a
+                  className="flex flex-col justify-between border-2"
+                  href={`/pokemon/${p.id}`}
+                  key={p.id}
+                >
+                  <img
+                    src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${p.image}`}
+                    alt={p.name}
+                  />
+                  <p className="text-center">{p.name}</p>
+                </a>
+              );
+            })}
         </div>
       </div>
     </>
